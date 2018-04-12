c369d625f6ed11cef17233f9b1389ce9d085cec9
Morgan Fudge <mfudge92@gmail.com>
2018-04-12 00:49:22 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,7 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def genre_name=(name)
+<<<<<<< HEAD
       genre = Genre.find_or_create_by(name: name)
       self.genre = genre
   end
@@ -20,6 +21,26 @@ class Song < ActiveRecord::Base
 
   def artist_name
       self.try(:artist).try(:name)
+=======
+    self.genre = Genre.find_or_create_by(name: name)
+    byebug
+  end
+
+  def genre_name
+    if self.genre
+      self.genre.name
+    end
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end
+>>>>>>> 9e8b82941597ed0583812fdbcb166d7af8d9db64
   end
 
 end
